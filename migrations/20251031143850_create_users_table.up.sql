87fcecc8cd12ab927d0f26d0c8a6ab82f8246203
Pedro-J-Kukul <pedro.j.kukul@gmail.com>
2025-10-31 23:52:55 -0600

    users migration

---
+++ migrations/20251031143850_create_users_table.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS "users" (
+    "id" BIGSERIAL PRIMARY KEY,
+    -- optional farmer_id for verification purposes
+    "farmer_id" TEXT UNIQUE,
+    -- user info
+    "email" TEXT UNIQUE NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "middle_name" TEXT,
+    "password_hash" BYTEA NOT NULL,
+    -- system related
+    "is_activated" BOOLEAN NOT NULL DEFAULT false,
+    "is_deleted" BOOLEAN NOT NULL DEFAULT false,
+    "is_verified" BOOLEAN NOT NULL DEFAULT false,
+    "version" INTEGER NOT NULL DEFAULT 1,
+    -- dates
+    "created_at" timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
+    "updated_at" timestamp WITH TIME ZONE NOT NULL DEFAULT now()
+);
